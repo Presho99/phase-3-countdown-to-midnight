e7346991d745074b0a1174599419e8eb7cc27533
Precious <Preshomondi@gmail.com>
2022-08-20 02:28:26 +0300

    commit tree dec30f0ce0dff5a665c6430433eaebcb7823ba08

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,20 @@
 #write your code here
 
-def countdown
+def countdown (x)
+    x = 10
+    while x >= 1
+        puts "#{x} SECOND(S)!"
+        x -= 1 
+    end
+    "HAPPY NEW YEAR!"
+end
+
+def countdown_with_sleep (x)
+    x = 10
+    while x >= 1
+        puts "#{x} SECOND(S)!"
+        sleep(5)
+        x -= 1 
+    end
+    "HAPPY NEW YEAR!"
 end
